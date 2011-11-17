deac40f729846cd4483ea1c72edfcfa0bd3b9433
Gabriel Kastenbaum <gabriel@localhost.(none)>
2011-11-17 03:16:45 +0100

    New version, now in Scala

--- foobar.scala
+++ foobar.scala
@@ -7,18 +7,40 @@ object foobar {
   def qix(n:Int):String={
     var n_in_cifers = n.toString.toList.map(_.toString.toInt)
     val contained = for (cifer <- n_in_cifers;
-		       x <- cifers_to_test
-		       if cifer == x ) yield yell(x)
+			 x <- cifers_to_test
+			 if cifer == x ) yield yell(x)
     val dividers = for (x <- cifers_to_test
-		       if n % x == 0 ) yield yell(x)
-    val result = contained:::dividers
+			if n % x == 0 ) yield yell(x)
+    val result = dividers:::contained
     result match {
       case List() => n.toString
       case l => l.mkString ("")
     }
   }
   val yell=Map ( 3 -> "Foo",
-		 5 -> "Bar",
-		 7 -> "Qix" )
+		5 -> "Bar",
+		7 -> "Qix" )
   val cifers_to_test = List (3,5,7)
 }
+
+object foobar_test {
+  def main(args: Array[String]) = tests
+
+  def tests={
+    val cases=Map(3 -> "FooFoo",
+		  4 -> "4",
+		  7 -> "QixQix",
+		  13 -> "Foo",
+		  15 -> "FooBarBar",
+		  33 -> "FooFooFoo",
+		  51 -> "FooBar",
+		  53 -> "BarFoo",
+		  64 -> "64",
+		  21 -> "FooQix")
+    for ( (input, expected) <- cases ) { 
+      assert (foobar.qix(input)==expected, "Fail for "+input+". Exp="+expected + " result="+foobar.qix(input)) 
+    }
+    println ("ok")
+  }
+}
+
